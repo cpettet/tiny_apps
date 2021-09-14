66cba59bcc0ddc0bc813fce1b94b091b6f44ffa4
cpettet <cpettet11@gmail.com>
2021-09-14 15:25:42 -0700

    looks pretty good, just working on footer

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const navLinks = document.querySelectorAll(".nav__link");
 const appNames = [];
+let index = 0;
 navLinks.forEach((navLink) => {
   appNames.push(navLink.id);
 });
 
-const appNameSpan = document.querySelector(".app-name");
-appNameSpan.innerText = appNames[0];
+appNames.push("grave");
+
+const appNameSpan = document.querySelector(".app__name");
+changeAppNameSpan();
+
+function changeAppNameSpan() {
+  appNameSpan.innerText = appNames[index];
+  if (appNames[index] === "grave") {
+    appNameSpan.classList.add("app__name-grave");
+    document.body.classList.add("app-grave")
+  } else {
+    appNameSpan.classList.remove("app__name-grave");
+    document.body.classList.remove("app-grave")
+  }
+}
+
+const appLeft = document.querySelector(".left");
+const appRight = document.querySelector(".right");
+
+appLeft.addEventListener("click", () => {
+  index = (index - 1) % appNames.length;
+  if (index < 0) index = appNames.length + index;
+  changeAppNameSpan();
+});
+
+appRight.addEventListener("click", () => {
+  index = Math.abs((index + 1) % appNames.length);
+  changeAppNameSpan();
+});
