e717fb71dfd101703530d0b1549507c44c6ee6b9
cpettet <cpettet11@gmail.com>
2021-10-03 11:56:51 -0700

    Styling for art. Changed formula for art.

--- tiny_art/art.js
+++ tiny_art/art.js
@@ -1,11 +1,11 @@
 const render = () => {
   const canvas = document.getElementById("art");
-  const ctx = canvas.getContext("2d");
+  const context = canvas.getContext("2d");
   for (let x = 0; x < 255; x++) {
     for (let y = 0; y < 255; y++) {
-      if ((x ^ y) % 10) {
-        ctx.fillRect(x * ((x ^ y) % 867), y * ((x ^ y) % 53.09), x, y);
-        ctx.fillStyle = `rgb(${(x ^ y) % 420}, ${y} ,${x} `;
+      if ((x * y) % 55) {
+        context.fillRect(x * ((x ^ y) % 4), y * ((x ^ y) % 8), 15, 16);
+        context.fillStyle = `rgb(${(x ^ y) % 23}, ${y} ,${42} `;
       }
     }
   }
